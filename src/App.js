99dbe6f82abd2abd5c18e66b0bb04a992f3e3ad4
mssohail-cloud <66189840+mssohail-cloud@users.noreply.github.com>
2020-06-03 07:57:16 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         
 
           <br /> <br />
           <h2>Updated by Muhammad Sohail</h2>
